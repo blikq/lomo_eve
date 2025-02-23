9552b803cd2435251f92afb0ba17c6e4cdb7ddf8
Blikq <blikqmain@gmail.com>
2025-02-24 00:09:10 +0100

    enable cors

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from eve import Eve
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_cors import CORS, cross_origin
 
 app = Eve()
+
+cors = CORS(app) # allow CORS for all domains on all routes.
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Initialize the limiter
 limiter = Limiter(
@@ -14,6 +18,7 @@ limiter = Limiter(
 # Apply a limit to the users POST endpoint (e.g., 5 requests per minute)
 @limiter.limit("5 per minute")
 @app.route('/people', methods=['POST'])
+@cross_origin()
 def limited_post():
     return app.view_functions['users']()
 
